eb218d87bd060bb3004d4a26070da3f549344fdc
NarayanAgarwal <abhayff754@gmail.com>
2026-07-28 23:48:28 +0530

    Configure dynamic web creation in build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ git clone https://github.com/flutter/flutter.git -b stable --depth 1
 # 2. Add Flutter to path
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# 3. Enable web support
+# 3. Enable web support and create web template files
+echo "Configuring Web support..."
 flutter config --enable-web
+flutter create . --platforms web --force
 
 # 4. Fetch dependencies
 echo "Getting pub packages..."
