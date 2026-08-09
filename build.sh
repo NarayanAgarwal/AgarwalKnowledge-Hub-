ad5fc67344212cca660ae8f8491f171638f50690
Agarwal Knowledge Hub <agarwalknowledgehubb@gmail.com>
2026-08-09 19:22:35 +0530

    Prevent flutter create overwrite in build.sh so no-cache headers apply to live build

--- build.sh
+++ build.sh
@@ -8,10 +8,9 @@ git clone https://github.com/flutter/flutter.git -b stable --depth 1
 # 2. Add Flutter to path
 export PATH="$PATH:`pwd`/flutter/bin"
 
-# 3. Enable web support and create web template files
+# 3. Enable web support
 echo "Configuring Web support..."
 flutter config --enable-web
-flutter create . --platforms web
 
 # 4. Fetch dependencies
 echo "Getting pub packages..."
